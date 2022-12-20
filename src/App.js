823d8010f4ac58b58446c1040c3558e0b0625343
carstennjones <121130186+carstennjones@users.noreply.github.com>
2022-12-20 16:43:38 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import Axios from 'axios'
-import React, {useState} from "react"
 import './App.css';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import {Menu} from "antd";
+
 
 function App() {
-  const [course,setCourse] = useState("")
-
-
-  const getCourses = () => {
-    Axios.get('https://canvas.colorado.edu/api/v1/courses/85828')
-      .then((response)=> {
-        console.log(response)
-        setCourse(response.data)
-      })
-  }
-
-  return (
-    <div className="App">
-      <button onClick={getCourses}>Touch</button> 
-      {course}
+   return ( 
+    <div>
+    <Header />
+    <div>
+      <SideMenu />
+      <Content />
     </div>
+    <Footer />
+    </div>
+   );
+}
+
+function Header() {
+  return <div>Header</div>
+}
+
+function Footer() {
+  return <div>Footer</div>
+}
+
+function SideMenu() {
+  const navigate = useNavigate();
+  return (<div style={{ display: "flex", flexDirection: "row" }}>
+    <Menu 
+    onClick={({key})=>{
+      if(key === "signout"){
+        //Add sign out feature
+      }else{
+        navigate(key)
+      }
+    }}
+
+    items={[
+      { label: "Home", key:"/" },
+      { label: "Dashboard", key:"/dashboard"},
+      { label: "Users List", key:"/usersList"},
+      { label: "Profile", key:"/profile"},
+      { label: "Signout",key:'signout', danger: true},
+    ]}
+    ></Menu>
+    </div>)
+}
+function Content() {
+  return (
+   <div>
+    <Routes>
+      <Route path="/" element={<div>Home</div>}></Route>
+      <Route path="/dashboard" element={<div>Dashboard</div>}></Route>
+      <Route path="/usersList" element={<div>Users List</div>}></Route>
+      <Route path="/profile" element={<div>Profile</div>}></Route>
+    </Routes>
+  </div>
   );
 }
 
